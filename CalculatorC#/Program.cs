b47036d6bc2970759b4e72bade5e5dbe73d6fd04
xVillainZx <seanpop23@gmail.com>
2023-10-08 22:48:24 -0400

    commit

--- CalculatorC#/Program.cs
+++ CalculatorC#/Program.cs
@@ -1,40 +1,99 @@
-﻿using System.Threading.Channels;
+﻿using System.ComponentModel.Design;
 
-Console.WriteLine("Please type your name");
-
-var name = Console.ReadLine();
 var date = DateTime.UtcNow;
 
-Console.WriteLine("-----------------------------------------------");
-Console.WriteLine($"Hello {name}! It's {date}. this is your math game. Thats great that you're improving yourself\n");
-Console.WriteLine($@"what game mode would you like to play today? Choose from the options below:
+string name = GetName();
+
+Menu(name);
+
+string GetName()
+{
+    Console.WriteLine("Please type your name");
+    var name = Console.ReadLine();
+    return name;
+}
+
+void Menu(string name)
+{
+    Console.WriteLine("-----------------------------------------------");
+    Console.WriteLine($"Hello {name}! It's {date}. this is your math game. Thats great that you're improving yourself\n");
+    Console.WriteLine($@"what game mode would you like to play today? Choose from the options below:
 a - Addition
 s - Subtraction
 m - Multiplication
 d - Division
 q - Quit the program :(");
-Console.WriteLine("-----------------------------------------------");
-var gameSelected = Console.ReadLine();
+    Console.WriteLine("-----------------------------------------------");
 
-if (gameSelected == "a")
-{
-    Console.WriteLine("Addition game mode selected.");
+    var gameSelected = Console.ReadLine();
+
+    switch (gameSelected.Trim().ToLower())
+    {
+        case "a":
+            AdditionGameMode("Addition game mode selected.");
+            break;
+        case "b":
+            SubtractionGameMode("Subtraction game mode selected.");
+            break;
+        case "m":
+            MultiplicationGameMode("Multiplication game mode selected.");
+            break;
+        case "d":
+            DivisionGameMode("Division game mode selected.");
+            break;
+        case "q":
+            Console.WriteLine("Goodbye");
+            Environment.Exit(1);
+            break;
+        default:
+            Console.WriteLine("Invalid input please try one of the displayed letters.");
+            Environment.Exit(1);
+            break;
+    }
 }
-else if (gameSelected == "s")
+
+void DivisionGameMode(string message)
 {
-    Console.WriteLine("Subtraction game mode selected.");
+    Console.WriteLine(message);
 }
-else if (gameSelected == "m")
+
+void MultiplicationGameMode(string message)
 {
-    Console.WriteLine("Multiplication game mode selected.");
+    Console.WriteLine(message);
 }
-else if (gameSelected == "d")
+
+void SubtractionGameMode(string message)
 {
-    Console.WriteLine("Division game mode selected.");
+    Console.WriteLine(message);
 }
-else
+
+void AdditionGameMode(string message)
 {
-    Console.WriteLine("Goodbye ;)")
-    Environment.Exit(1);
+    Console.WriteLine(message);
+
+    var random = new Random();
+    var score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} + {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber + secondNumber)
+        {
+            Console.WriteLine("Correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect.");
+        }
+        if (i == 4) Console.WriteLine($"Your final score is {score}");
+    }
 }
-Console.ReadLine();
