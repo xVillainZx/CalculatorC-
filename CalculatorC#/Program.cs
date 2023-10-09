e19fed02a1694b9e033003217ccf4bf2878b8058
xVillainZx <seanpop23@gmail.com>
2023-10-08 23:46:33 -0400

    commit

--- CalculatorC#/Program.cs
+++ CalculatorC#/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.Design;
+using static System.Formats.Asn1.AsnWriter;
 
 var date = DateTime.UtcNow;
 
@@ -16,7 +17,7 @@ string GetName()
 void Menu(string name)
 {
     Console.WriteLine("-----------------------------------------------");
-    Console.WriteLine($"Hello {name}! It's {date}. this is your math game. Thats great that you're improving yourself\n");
+    Console.WriteLine($"Hello {name}! It's {date}. this is your math game. Thats great that you're improving yourself!\n");
     Console.WriteLine($@"what game mode would you like to play today? Choose from the options below:
 a - Addition
 s - Subtraction
@@ -30,16 +31,16 @@ q - Quit the program :(");
     switch (gameSelected.Trim().ToLower())
     {
         case "a":
-            AdditionGameMode("Addition game mode selected.");
+            AdditionGameMode("Addition game mode.");
             break;
-        case "b":
-            SubtractionGameMode("Subtraction game mode selected.");
+        case "s":
+            SubtractionGameMode("Subtraction game mode.");
             break;
         case "m":
-            MultiplicationGameMode("Multiplication game mode selected.");
+            MultiplicationGameMode("Multiplication game mode.");
             break;
         case "d":
-            DivisionGameMode("Division game mode selected.");
+            DivisionGameMode("Division game mode.");
             break;
         case "q":
             Console.WriteLine("Goodbye");
@@ -54,17 +55,103 @@ q - Quit the program :(");
 
 void DivisionGameMode(string message)
 {
-    Console.WriteLine(message);
+    for (int i = 0; i <5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine(message);
+
+        var divisionNumbers = GetDivisionNumbers();
+        var firstNumber = divisionNumbers[0];
+        var secondNumber = divisionNumbers[1];
+        var score = 0;
+
+        Console.WriteLine($"{firstNumber} / {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber / secondNumber)
+        {
+            Console.WriteLine("Correct! Press any key to continue.");
+            score++;
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Incorrect. Press any key to continue.");
+            Console.ReadLine();
+        }
+        if (i == 4) Console.WriteLine($"Your final score is {score}");
+    }
+
 }
 
 void MultiplicationGameMode(string message)
 {
     Console.WriteLine(message);
+    var random = new Random();
+    var score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine(message);
+
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} * {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber * secondNumber)
+        {
+            Console.WriteLine("Correct! Press any key to continue.");
+            score++;
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Incorrect. Press any key to continue.");
+            Console.ReadLine();
+        }
+        if (i == 4) Console.WriteLine($"Your final score is {score}");
+    }
 }
 
 void SubtractionGameMode(string message)
 {
     Console.WriteLine(message);
+    var random = new Random();
+    var score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine(message);
+
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} - {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber - secondNumber)
+        {
+            Console.WriteLine("Correct! Press any key to continue.");
+            score++;
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Incorrect. Press any key to continue.");
+            Console.ReadLine();
+        }
+        if (i == 4) Console.WriteLine($"Your final score is {score}");
+    }
 }
 
 void AdditionGameMode(string message)
@@ -79,6 +166,9 @@ void AdditionGameMode(string message)
 
     for (int i = 0; i < 5; i++)
     {
+        Console.Clear();
+        Console.WriteLine(message);
+
         firstNumber = random.Next(1, 9);
         secondNumber = random.Next(1, 9);
 
@@ -87,13 +177,37 @@ void AdditionGameMode(string message)
 
         if (int.Parse(result) == firstNumber + secondNumber)
         {
-            Console.WriteLine("Correct!");
+            Console.WriteLine("Correct! Press any key to continue.");
             score++;
+            Console.ReadLine();
         }
         else
         {
-            Console.WriteLine("Incorrect.");
+            Console.WriteLine("Incorrect. Press any key to continue.");
+            Console.ReadLine();
         }
         if (i == 4) Console.WriteLine($"Your final score is {score}");
     }
+}
+
+int[] GetDivisionNumbers()
+{
+    var random = new Random();
+    var firstNumber = random.Next(0, 99);
+    var secondNumber = random.Next(0, 99);
+
+    var result = new int[2];
+
+    while (firstNumber % secondNumber != 0)
+    {
+        firstNumber = random.Next(1, 99);
+        secondNumber = random.Next(1, 99);
+    }
+
+    result[0] = firstNumber;
+    result[1] = secondNumber;
+
+    Console.WriteLine(result);
+
+    return result;
 }
